d86f43424631ee177a99b8dccc30d87ce2f1c0e4
abhisheknaik24 <development.abhi2000@gmail.com>
2023-02-09 16:23:10 +0530

    changes

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,15 +1,8 @@
 import multer from 'multer';
-import fs from 'fs';
-
-const dir = './public/images';
-
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, dir);
+    callback(null, './public/images');
   },
   filename: function (req, file, callback) {
     callback(null, file.originalname);
